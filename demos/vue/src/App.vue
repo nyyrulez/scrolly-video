514a230e272d8ea5d9ed927e5644b70d1efb186d
RJ <120344418+RandyZJin@users.noreply.github.com>
2025-11-20 21:13:54 +0000

    change path

--- demos/vue/src/App.vue
+++ demos/vue/src/App.vue
@@ -4,7 +4,7 @@ import ScrollyVideo from 'scrolly-video/dist/ScrollyVideo.vue';
 
 <template>
 <div class="scrolly-video-container">
-  <ScrollyVideo src="public/test.mp4"" />
+  <ScrollyVideo src="./public/videos/test.mp4" />
 </div>
 </template>
 
